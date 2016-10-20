8dbe2178b6e36e5ad8e835de9d34a48484859725
Jace Ju <jaceju@gmail.com>
2016-10-20 12:05:20 +0800

    加入 laravel-echo 套件與 laravel-echo-server 的設定

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,14 @@
 import Vue from 'vue';
+import Echo from 'laravel-echo';
+import SocketIO from 'socket.io-client';
 import CurrentTime from './components/CurrentTime.vue';
+
+window.io = SocketIO;
+
+window.Echo = new Echo({
+  broadcaster: 'socket.io',
+  host: 'http://example-dashboard.dev:6001'
+});
 
 new Vue({
   el: '#app',
